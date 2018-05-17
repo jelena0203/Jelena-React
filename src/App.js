6753a1b0a7d44f530769ffa36e296553751d8f6f
jelena0203 <jelena.0203@gmail.com>
2018-05-17 10:32:08 +0200

    inicijalni

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Flat from'./components/flat';
+
+class App extends Component {
+  render() {
+
+      const flat = {
+        "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
+        "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat2.jpg",
+        "price": 164,
+        "priceCurrency": "EUR",
+        "lat": 48.884211,
+        "lng": 2.346890
+      };
+  return (
+       // <div>
+       //   <Flat flat={flat}/>
+       //   <Flat flat={flat}/>
+       // </div>
+      <div className="app">
+        <div className="main">
+            <div className="search">
+            </div>
+            <div className="flats">
+            </div>
+        </div>
+        <div className="map">
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
